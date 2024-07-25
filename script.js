f51d5d612cc79561855ea98f5b39602ee202dfec
Guilherme Barbosa <https://github.com/Gui806/Gui806.github.io>
2024-07-25 03:14:34 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() { var pageTitle = document.title;
+    if (pageTitle === "Sobre MIM"){
+        alert("Seja Bem-vindo 1ªPágina");
+    } else if (pageTitle === "Projetos") {
+        alert("Seja Bem-vindo as meus Projetos");
+    } 
+    };
